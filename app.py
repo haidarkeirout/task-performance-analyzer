fada1a2981218ffdfdc34814b5c6755b8b83762e
haidarkeirout <haidar.4keirout@gmail.com>
2026-09-22 16:39:20 +0300

    Tighten header logo spacing

--- app.py
+++ app.py
@@ -1397,8 +1397,10 @@ from automation_ui import invalidate_selection, require_sign_in, render_collecti
 settings = require_sign_in()
 
 LOGO_PATH = ROOT_DIR / "assets" / "bidayah_logo_white.png"
-if LOGO_PATH.exists():
-    st.image(str(LOGO_PATH), width=190)
+HEADER_LOGO_PATH = ROOT_DIR / "assets" / "bidayah_logo_header.png"
+display_logo_path = HEADER_LOGO_PATH if HEADER_LOGO_PATH.exists() else LOGO_PATH
+if display_logo_path.exists():
+    st.image(str(display_logo_path), width=190)
 
 st.title(APP_TITLE)
 analysis_mode_label = st.radio(
